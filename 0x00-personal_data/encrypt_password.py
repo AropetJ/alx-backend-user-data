ad62d4289f117f57c8a5b39e90431802f26c48c1
AropetJ <aropetjoel32@gmail.com>
2024-02-08 14:45:17 +0100

    Attempted task 5

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""encrypt password module for encrypting passwords.
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """
+    Hashes a password using a random salted algorithm.
+    Returns:
+        bytes: The salted, hashed password, which is a byte string.
+    """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """validate that the provided password matches the hashed password.
+    Returns:
+        bool: True if the hashed password matches the given password, False otherwise.
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
