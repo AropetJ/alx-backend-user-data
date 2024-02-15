3cdf67f124529a51b988bb2d8ae451d4987cdd37
AropetJ <aropetjoel32@gmail.com>
2024-02-15 14:14:23 +0100

    Attempted task 3

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 ''' Module of Auth views
 '''
+import re
 from flask import request
 from typing import List, TypeVar
 
@@ -11,7 +12,18 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         ''' require_auth method
         '''
-        return False
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            for exclusion_path in map(lambda x: x.strip(), excluded_paths):
+                pattern = ''
+                if exclusion_path[-1] == '*':
+                    pattern = '{}.*'.format(exclusion_path[0:-1])
+                elif exclusion_path[-1] == '/':
+                    pattern = '{}/*'.format(exclusion_path[0:-1])
+                else:
+                    pattern = '{}/*'.format(exclusion_path)
+                if re.match(pattern, path):
+                    return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         ''' authorization_header method
