6fe7d4299381ed69d6d5d340c2fd215eb780e612
AropetJ <aropetjoel32@gmail.com>
2024-02-15 14:46:02 +0100

    Attempted task 6

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -9,6 +9,7 @@ from flask_cors import (CORS, cross_origin)
 import os
 
 from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 
 
 app = Flask(__name__)
@@ -17,6 +18,8 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 if os.getenv('AUTH_TYPE') == 'auth':
     auth = Auth()
+if os.getenv('AUTH_TYPE') == 'basic_auth':
+    auth = BasicAuth()
 
 
 @app.errorhandler(404)
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+''' Module of Auth views
+'''
+from .auth import Auth
+
+class BasicAuth(Auth):
+    '''BasicAuth class
+    '''
+    pass
