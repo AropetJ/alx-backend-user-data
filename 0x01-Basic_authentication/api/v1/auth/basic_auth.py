3aa021f5cc71a1ffa22e980a01f5a32c07102674
AropetJ <aropetjoel32@gmail.com>
2024-02-15 15:01:01 +0100

    Attempted task 8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 ''' Module of Auth views
 '''
+import base64
+import binascii
 from .auth import Auth
 
 
@@ -19,3 +21,18 @@ class BasicAuth(Auth):
         if not authorization_header.startswith('Basic '):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str
+                                           ) -> str:
+        ''' decode_base64_authorization_header method
+        '''
+        if type(base64_authorization_header) == str:
+            try:
+                res = base64.b64decode(
+                    base64_authorization_header,
+                    validate=True,
+                )
+                return res.decode('utf-8')
+            except (binascii.Error, UnicodeDecodeError):
+                return None
