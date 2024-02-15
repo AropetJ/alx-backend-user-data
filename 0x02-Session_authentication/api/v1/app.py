205fa95bec5b227fef0874674b363240290d0ea6
AropetJ <aropetjoel32@gmail.com>
2024-02-15 15:57:38 +0100

    Attempted task 0

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -52,16 +52,12 @@ def before_request() -> None:
             '/api/v1/status/',
             '/api/v1/unauthorized/',
             '/api/v1/forbidden/',
-            '/api/v1/auth_session/login/',
         ]
         if auth.require_auth(request.path, excluded_paths):
-            user = auth.current_user(request)
-            if auth.authorization_header(request) is None and \
-                    auth.session_cookie(request) is None:
+            if not auth.authorization_header(request):
                 abort(401)
-            if user is None:
+            if not auth.current_user(request):
                 abort(403)
-            request.current_user = user
 
 
 if __name__ == "__main__":
