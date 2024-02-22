81a2c08b2e2c4981b521b4b6e8404a6713652bd5
AropetJ <aropetjoel32@gmail.com>
2024-02-22 13:53:03 +0100

    Added the update password method

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -91,5 +91,24 @@ def get_reset_password_token() -> str:
         abort(403)
 
 
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
+def update_password() -> str:
+    """PUT /reset_password
+    JSON body:
+      - email: string -> user email
+      - reset_token: string -> reset token
+      - new_password: string -> new user password
+    Return: {"email": "<user email>", "message": "Password updated"}
+    """
+    email = request.form.get('email')
+    reset_token = request.form.get('reset_token')
+    new_password = request.form.get('new_password')
+    try:
+        AUTH.update_password(reset_token, new_password)
+        return jsonify({"email": email, "message": "Password updated"})
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
